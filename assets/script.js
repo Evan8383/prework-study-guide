c9a7b0b53b8fb0d16fe0158b33b4a38e97bd2dfb
Evan Bennett <138387806+Evan8383@users.noreply.github.com>
2023-07-04 11:45:01 -0400

    Updated script.js with functions to view and select topics

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,25 @@
-/*
-const selectTopic = setTimeout(() =>{
-    let topic = prompt("Select a topic to study")
-    if (topic === 'HTML') {
+let topics = ['HTML', 'CSS', 'Git', 'JavaScript']
+let randomTopic = topics[Math.floor(Math.random() * topics.length)]
+
+function listTopics(){
+    for(let i = 0; i < topics.length; i++){
+        console.log(topics[i])
+    }
+}
+function selectTopic(){
+    if (randomTopic === 'HTML') {
         console.log("Let's study HTML!");
-    } else if (topic === 'CSS') {
+    } else if (randomTopic === 'CSS') {
         console.log("Let's study CSS!");
-    } else if (topic === 'Git') {
+    } else if (randomTopic === 'Git') {
         console.log("Let's study Git!");
-    } else if (topic === 'JavaScript') {
+    } else if (randomTopic === 'JavaScript') {
         console.log("Let's study JavaScript!");
     } else {
         console.log('Please try again!');
     }
-}, 1000);
-*/
-
-let topics = ['HTML', 'CSS', 'Git', 'JavaScript']
-for(let i = 0; i < topics.length; i++){
-    console.log(topics[i])
 }
+console.log('Here are the topics we learned through Prework: ')
+listTopics()
+console.log('What topic would you like to study first?')
+selectTopic()
