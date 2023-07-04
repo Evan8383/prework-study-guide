5754b13f108b756f090787d3fbb91ac42266c4fa
Evan Bennett <138387806+Evan8383@users.noreply.github.com>
2023-07-04 11:21:45 -0400

    Added JS file to practive for loops and conditionals

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+/*
+const selectTopic = setTimeout(() =>{
+    let topic = prompt("Select a topic to study")
+    if (topic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (topic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (topic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (topic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
+}, 1000);
+*/
+
+let topics = ['HTML', 'CSS', 'Git', 'JavaScript']
+for(let i = 0; i < topics.length; i++){
+    console.log(topics[i])
+}
